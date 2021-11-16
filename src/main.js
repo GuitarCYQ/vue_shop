eec8871676443483f2a2b5e993a891e53edd94e9
GuitarCYQ <1159543426@qq.com>
2021-11-16 11:36:03 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式
+import './assets/css/global.css'
+
+// 导入请求http包 axios
+import axios from 'axios'
+// 配置请求路径
+axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/'
+// prototype原型对象
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
